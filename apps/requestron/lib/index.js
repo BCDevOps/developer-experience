bffde932aca8b92cc8a0b3227ee2bb9cee15cf2c
Cailey Jones <cailey.jones@gov.bc.ca>
2019-12-02 10:54:31 -0800

    milestone update

--- apps/requestron/lib/index.js
+++ apps/requestron/lib/index.js
@@ -36,30 +36,32 @@ var __generator = (this && this.__generator) || function (thisArg, body) {
     }
 };
 var estimate_1 = require("./lib/estimate");
+var milestone_1 = require("./lib/milestone");
 module.exports = function (app) {
     app.on('issues.opened', issueOpened);
     function issueOpened(context) {
         return __awaiter(this, void 0, void 0, function () {
-
             var newIssue, err_1;
             return __generator(this, function (_a) {
                 switch (_a.label) {
                     case 0:
-                        _a.trys.push([0, 2, , 3]);
+                        _a.trys.push([0, 3, , 4]);
                         newIssue = context.issue();
                         console.log(newIssue.number);
                         return [4 /*yield*/, estimate_1.setEstimate(newIssue.number)];
                     case 1:
                         _a.sent();
-                        return [3 /*break*/, 3];
+                        return [4 /*yield*/, milestone_1.setMilestone(context)];
                     case 2:
+                        _a.sent();
+                        return [3 /*break*/, 4];
+                    case 3:
                         err_1 = _a.sent();
                         throw Error('Unable to handle issue: ' + err_1);
-                    case 3: return [2 /*return*/];
+                    case 4: return [2 /*return*/];
                 }
             });
         });
     }
-
 };
 //# sourceMappingURL=index.js.map
--- apps/requestron/lib/lib/estimate.d.ts
+++ apps/requestron/lib/lib/estimate.d.ts
@@ -1,3 +1,1 @@
-
 export declare function setEstimate(issueID: number): Promise<boolean>;
-
