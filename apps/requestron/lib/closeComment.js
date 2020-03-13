db293f689525bf86440b9419a04d3d8f928e2e9f
Cailey Jones <cailey.jones@gov.bc.ca>
2020-03-13 15:29:57 -0700

    added quota-type and new-request-type to the dictionary of closing comment details

--- apps/requestron/lib/closeComment.js
+++ apps/requestron/lib/closeComment.js
@@ -6,19 +6,27 @@ const axios = require('axios');
 
 const commentDetails = {
     'annotation-update': 'You can view your project annotations by running the command `oc describe namespace [namespace-name]`.',
-    'artifactory-repo': 'You can find the custom resource for your repo in the listed tools namespace by running `oc get artifactoryrepos`.' +
+    'artifactory-repo': 'You can find the custom resource for your repo in the listed tools namespace by running `oc get artifactoryrepos`. ' +
         'For information on the status of the resource, run `oc describe artifactoryrepo [repo-name]`.',
     'documize-space': 'You should expect to receive an invitation to your Documize space in your email inbox.',
     'duplicate': 'This task will not be completed because it is a duplicate of another task.',
-    'github-membership': 'The invitation should appear in the relevant user\'s inbox very soon. Please ensure that they\'re checking the correct inbox.' +
-        'The invitation will show up in the inbox of whatever email address is listed as their primary address in their github profile.' +
+    'github-membership': 'The invitation should appear in the relevant user\'s inbox very soon. Please ensure that they\'re checking the correct inbox. ' +
+        'The invitation will show up in the inbox of whatever email address is listed as their primary address in their github profile. ' +
         'Please note that this request ONLY provides access to the `bcgov` and/or `bcgov-c` organizations. It does not provide access to Openshift. ' +
         'If you require that, please open another issue [here](https://github.com/BCDevOps/devops-requests/issues/new?assignees=caggles%2C+ShellyXueHan&labels=openshift-access&template=openshift_user_access_request.md&title=)',
+    'github-repo': 'Assuming this task was approved, you will find your new repo in bcgov-c with the requested name, unless otherwise indicated. ' +
+        'The requested administrator of this repo will be the only one with access upon creation, and should add additional collaborators as required. ' +
+        'If you need to add these additional collaborators to the bcgov-c org, please create another request [here](https://github.com/BCDevOps/devops-requests/issues/new?assignees=caggles%2C+ShellyXueHan&labels=github-membership&template=github_user_access_request.md&title=)',
     'keycloak-realm': 'Please follow the instructions above to use realm-o-matic.',
-    'openshift-access': 'The user in question should receive an invitation to the BCDevOps organization on GitHub. They must accept this invitation before they will be able to access Openshift.' +
+    'openshift-access': 'The user in question should receive an invitation to the BCDevOps organization on GitHub. They must accept this invitation before they will be able to access Openshift. ' +
         'The invitation will show up in the inbox of whatever email address is listed as their primary address in their github profile.',
-    'openshift-project-set': 'Assuming this task was approved, you will find your project set by logging into the Openshift console and navigating to the Application Console.' +
-        'There, you will find a list of all projects to which you have access. The technical steward will have admin access and can add new users to the projects as required.'
+    'openshift-project-set': 'Assuming this task was approved, you will find your project set by logging into the Openshift console and navigating to the Application Console. ' +
+        'There, you will find a list of all projects to which you have access. The technical steward will have admin access and can add new users to the projects as required.',
+    'quota-update': 'Assuming this request has been approved, the closure of this ticket means that the platform services team has submitted this change to be implemented on the platform by DXCAS. ' +
+        'It is *not* completed yet, and you may need to wait for a while longer before you see the change in your namespace. ' +
+        'If you are concerned by how long it is taking, please ask for further information on rocketchat.',
+    'new-request-type': 'Assuming this request has been approved, you will now find the new request type available for submissions. ' +
+        'Please note that, if you require action on that new request type yourself, you will need to submit a request of that type as well - the closure of this ticket does not indicate anything beyond the existence of the new request type.'
 };
 const instance = axios.create({
   baseURL: 'https://api.github.com/',
