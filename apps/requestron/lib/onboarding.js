de70eef7ca28ea2178390ac9dc7c209af90e1b2a
Cailey Jones <cailey.jones@gov.bc.ca>
2020-04-14 12:20:47 -0700

    add onboarding link comment

---
+++ apps/requestron/lib/onboarding.js
@@ -0,0 +1,43 @@
+require('probot');
+const axios = require('axios');
+/**
+ * Create a comment on close that includes some useful instructions/details
+ */
+
+const instance = axios.create({
+  baseURL: 'https://api.github.com/',
+  timeout: 10000,
+  headers: {'Authorization': 'token ' + process.env.GITHUB_TOKEN}
+});
+
+module.exports = async function createOnboardingComment(context) {
+    try {
+
+        const closingIssue = context.issue();
+        const getResponse = await instance.get('repos/' + closingIssue.owner + '/' + closingIssue.repo + '/issues/' + closingIssue.number + '/labels');
+
+        // make the common open statement of the closing comment
+        let commentContent = 'Thank you for requesting a new Openshift Project Set! We look forward to having you on our platform!\n\n' +
+            'If you\'re new to the platform (or even if you\'ve been around a while and just need a refresher!), please read over ' +
+            'our [Onboarding Journey](https://developer.gov.bc.ca/Onboarding-Journey/Welcome-to-our-Platform-Community!).' +
+            'It outlines what we expect of you and what you can expect from us. Please take a look!';
+
+        // add the unique closing comment lines for each appropriate label
+        // this probably shouldn't result in multiple lines at the moment, but just in case we want to add different stuff for the "approved" vs "rejected" labels later.
+        for (let i = 0; i < getResponse.data.length; i++) {
+            if (getResponse.data[i]['name'] == 'openshift-project-set') {
+                const issueComment = context.issue({ body: commentContent });
+                await context.github.issues.createComment(issueComment);
+            }
+        }
+
+        // post the closing comment
+        return true;
+
+    } catch (err) {
+
+        throw Error('Unable to handle issue in closing comment: ' + err)
+
+    }
+};
+
