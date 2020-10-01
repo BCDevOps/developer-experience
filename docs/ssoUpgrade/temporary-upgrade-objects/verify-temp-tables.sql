255ae16baab6f5271ac88e943f93469e85547113
Shelly Han <shelly.han@gov.bc.ca>
2020-10-01 13:59:12 -0700

    SSO 7.4 upgrade (#575)
    * initial notification for sso upgrade
    
    * updated timeline and provided overview of upgrade tasks
    
    * detailed steps for sso 7.4 upgrade
    
    * templates needed during maunal upgrade process
    
    * added rollback plan
    
    * splite sso upgrade notification
    
    * making dates more clear
    
    * remove notification message from upgrade doc
    
    * Update docs/ssoUpgrade/sso-7.4-upgrade-notification.md
    
    Co-authored-by: Jeff <22400069+jefkel@users.noreply.github.com>
    
    * Update docs/ssoUpgrade/sso-7.4-upgrade-notification.md
    
    Co-authored-by: Patrick Simonian <Patrick.Simonian@gov.bc.ca>
    
    * Update docs/ssoUpgrade/sso-7.4-upgrade-notification.md
    
    Co-authored-by: Jeff <22400069+jefkel@users.noreply.github.com>
    
    * Update docs/ssoUpgrade/sso-7.4-upgrade-notification.md
    
    Co-authored-by: Jeff <22400069+jefkel@users.noreply.github.com>
    
    * Update docs/ssoUpgrade/sso-7.4-upgrade-notification.md
    
    Co-authored-by: Jeff <22400069+jefkel@users.noreply.github.com>
    
    * Update docs/ssoUpgrade/sso-7.4-upgrade-notification.md
    
    Co-authored-by: Jeff <22400069+jefkel@users.noreply.github.com>
    
    * Update docs/ssoUpgrade/sso-7.4-upgrade-notification.md
    
    Co-authored-by: Jeff <22400069+jefkel@users.noreply.github.com>
    
    * update upgrade steps with more details
    
    * Update docs/ssoUpgrade/sso-7.4-upgrade-notification.md
    
    * Update docs/ssoUpgrade/sso-7.4-upgrade-notification.md
    
    * update to only swtich dev-sso storage back to gluster
    
    * added in temporaty objects pre-generated
    
    * update steps with reference to the pregenerated templates
    
    * Update sso-7.4-upgrade-notification.md
    
    * wording changes
    
    * added more details on upgrade steps
    
    * added missing istag import step
    
    * update steps
    
    * added steps missing, found from test upgrade run
    
    * misc updates
    
    * test local user needs a role
    
    * Added sql for dropping multiple tables with the same prefix
    
    * update SQL script and instructions
    
    * update manual upgrade steps
    
    Co-authored-by: Jeff <22400069+jefkel@users.noreply.github.com>
    Co-authored-by: Patrick Simonian <Patrick.Simonian@gov.bc.ca>
    Co-authored-by: Olena Mitovska <olena.mitovska@gmail.com>
    Co-authored-by: Patrick Simonian <patricksimonian@gmail.com>
    Co-authored-by: Alan Dodge <alan.dodge@gov.bc.ca>

---
+++ docs/ssoUpgrade/temporary-upgrade-objects/verify-temp-tables.sql
@@ -0,0 +1,24 @@
+/* 
+  Change <TABLE_PREFIX> to prefix to drop
+  To verify: uncomment line 19 (RAISE NOTICE) and comment line 20
+  To execute: comment line 19 and uncomment line 20 (EXECUTE)
+*/
+
+DO
+$do$
+DECLARE
+   _tbl text;
+BEGIN
+FOR _tbl  IN
+    SELECT quote_ident(table_schema) || '.'
+        || quote_ident(table_name)      -- escape identifier and schema-qualify!
+    FROM   information_schema.tables
+    WHERE  table_name LIKE 'ossso' || '%'  -- your table name prefix, set as "ossso"
+    AND    table_schema NOT LIKE 'pg\_%'    -- exclude system schemas
+LOOP
+   RAISE NOTICE '%',
+-- EXECUTE
+  'DROP TABLE ' || _tbl;  -- see below
+END LOOP;
+END
+$do$;
